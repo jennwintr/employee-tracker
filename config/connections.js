12d56797e6e3e37131c665ed9681e5598fcab75d
Jennifer Winter <jennwintr@gmail.com>
2024-01-10 10:11:48 -0500

    connections fix

--- config/connections.js
+++ config/connections.js
@@ -1,14 +1,13 @@
-const mysql = require ("mysql2")
+const mysql = require ("mysql2");
+
+require('dotenv').config();
 
 const connections = mysql.createConnection ({
     host: "localhost",
+    port: 3306,
     user: "root",
     password: "",
-    database: "employee_db"
-})
-
-connections.connect(()=>{
-    console.log("connected")
-})
+    database: "employees"
+});
 
 module.exports = connections;
