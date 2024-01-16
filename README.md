50e8fad25e206473485af03548ec523a04ae6f81
Jennifer Winter <jennwintr@gmail.com>
2024-01-16 12:48:08 -0500

    debug

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# employee-tracker
+
+## Title
+Employee Tracker
+
+## Description
+This is a command-line application to manage a company's employee database, using Node.js, Inquirer, and MySQL.
+
+## GitHub Repo Link
+[https://github.com/jennwintr/employee-tracker](https://github.com/jennwintr/employee-tracker)
+
+## Video Link
--- javascript/validate.js
+++ javascript/validate.js
@@ -1,16 +1,30 @@
-const validator = require('validator');
+// Import the 'validator' module to perform string and number validation
+const validator = require("validator");
 
+// Create an object named 'validate' to hold validation functions
 const validate = {
+  // Validation function for non-empty strings
   validateString(str) {
-    return str !== '' || 'Please enter a valid response!';
+    // Check if the string is not empty, return true
+    // If the string is empty, return an error message
+    return str !== "" ? true : "Please enter a valid response!";
   },
+
+  // Validation function for numeric values (assumed to be salary)
   validateSalary(num) {
+    // Use the 'isDecimal' function from 'validator' to check if the number is a decimal
+    // If it is a decimal, return true, otherwise return an error message
     if (validator.isDecimal(num)) return true;
-    return 'Please enter a valid salary!';
+    return "Please enter a valid salary!";
   },
+
+  // Function to check if two strings are equal
   isSame(str1, str2) {
+    // Check if the two strings are equal, return true if they are
+    // If they are not equal, the function implicitly returns 'undefined'
     if (str1 === str2) return true;
-  }
+  },
 };
 
+// Export the 'validate' object, making its functions accessible to other modules
 module.exports = validate;
