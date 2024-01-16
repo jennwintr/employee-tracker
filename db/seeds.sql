a3c791702ede1aa5aa522525729fb1c025ce699d
Jennifer Winter <jennwintr@gmail.com>
2024-01-16 10:27:15 -0500

    seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,10 +1,10 @@
 USE employees;
 
-INSERT INTO department (department_name)
-VALUES ("engineering"), ("managment"), ("legal");
+INSERT INTO department(department_name)
+VALUES("Engineering"), ("Sales"), ("Finance"), ("Legal"), ("Marketing");
 
-INSERT INTO role (title, salary, department_id)
-VALUES ("engineer", 80000, 1), ("manager", 30000, 2), ("lawyer", 50000, 3);
+INSERT INTO role(title, salary, department_id)
+VALUES("Engineer", 85000, 1), ("Senior Engineer", 125000, 1), ("CFO", 350000, 3), ("Chief Counsel", 300000, 4);
 
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Jennifer", "Winter", 1, 2), ("Nora", "Chung", 2, null), ("Grey", "Chung", 3, 2);
+INSERT INTO employee(first_name, last_name, role_id, manager_id)
+VALUES ('Kata', 'Ra', 1, 2), ('Toph', 'Beifong', 1, null), ('Ty', 'Lee', 1, 2), ('Princess', 'Yue', 2, 2), ('On', 'Ji', 4, null);
